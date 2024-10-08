2ced216c88fe2b14abf8b68ebfef1069ab79a786
vladg <vlad.g.atom@gmail.com>
2024-10-08 23:55:22 +0600

    v0.2 added shops and reviews

--- manas_app/shops/admin.py
+++ manas_app/shops/admin.py
@@ -1,6 +1,6 @@
+# shops/admin.py
 from django.contrib import admin
 from .models import Shop, Review
 
 admin.site.register(Shop)
 admin.site.register(Review)
-
--- manas_app/shops/models.py
+++ manas_app/shops/models.py
@@ -1,3 +1,4 @@
+# shops/models.py
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Avg
