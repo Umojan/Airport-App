78a374374b91e4a21df751ec8c2cea10734b515e
Vlad G <vlad.g.atom@gmail.com>
2024-11-07 20:13:50 +0600

    v0.3 Added schedule model + filters in api

--- django_manas/flights/views.py
+++ django_manas/flights/views.py
@@ -1,14 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Flight
 from .serializers import FlightSerializer
+from .filters import FlightFilter
 
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        is_arrival = self.request.query_params.get('is_arrival')
-        if is_arrival is not None:
-            queryset = queryset.filter(is_arrival=is_arrival == 'true')
-        return queryset
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = FlightFilter  # Use the custom FlightFilter
+    ordering_fields = ['time']  # Enables ordering by time
